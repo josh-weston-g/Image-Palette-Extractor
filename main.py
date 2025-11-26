3f3566047251e6abf2afd01b684e1f6c85f05c75
Josh Weston <76183891+josh-weston-g@users.noreply.github.com>
2025-11-26 15:03:18 +0000

    Set up basic running program. Takes file location as input and number of clusters as input and returns n number of rgb values based on provided image file.

---
+++ main.py
@@ -0,0 +1,49 @@
+from PIL import Image
+import numpy as np
+from sklearn.cluster import KMeans
+
+# Open image file
+while True:
+    imagePath = input("Enter path to image file: ")
+    try:
+        im = Image.open(imagePath)
+        break
+    except FileNotFoundError:
+        print("File not found. Please enter a valid file path.")
+    except Exception as e:
+        print(f"An error occurred: {e}. Please try again.")
+
+# Display image format, size, and mode - used for debugging
+print(f"\nFile type: {im.format} \nImage size: {im.size} \nImage mode: {im.mode}")
+
+# Show the image - used for debugging
+# im.show()
+
+# Resize image to speed up cluster processing - we use thumbnail to maintain aspect ratio
+# Longest side will be 200 pixels
+im.thumbnail((200, 200))
+print(f"Resized image size: {im.size}\n")
+
+# Get list of pixels from image
+# Returns a list of tuples representing each RGB value
+pixelsList = list(im.getdata())
+
+# Convert list of tuples to a 2D numpy array
+pixels = np.array(pixelsList)
+
+# Number of colors to reduce the image to
+while True:
+    try:
+        numColors = int(input("Enter number of colors to redudce the image to (1-20): "))
+        if numColors < 1 or numColors > 20:
+            print("Please enter a number between 1 and 20.")
+            continue
+        break
+    except ValueError:
+        print("Invalid input. Please enter a number between 1 and 20.")
+
+# Perform KMeans clustering to reduce the number of colors
+kmeans = KMeans(n_clusters=numColors, random_state=42).fit(pixels)
+# Get the cluster centers (the representative colors) as integers (normally returns floats)
+colors = kmeans.cluster_centers_.astype(int)
+print(f"\n{colors}")
